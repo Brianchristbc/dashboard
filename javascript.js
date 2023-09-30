acc784b249f253432c8e87f666460be200bc008d
Brianchristbc <138142173+Brianchristbc@users.noreply.github.com>
2023-09-30 23:32:07 +0800

    Add a pause button to audio easter eggs

--- javascript.js
+++ javascript.js
@@ -2,9 +2,15 @@ const root = document.documentElement;
 
 const theme = document.querySelector('.set-theme');
 const alertButton = document.querySelector('.alert-button');
-const themeButton = document.querySelectorAll('button');
-const audio = document.querySelector('.alert-sound');
 const themeSong = document.querySelector('.theme-song');
+const stanley = document.querySelector('.stanley');
+const dwight = document.querySelector('.dwight');
+const jim = document.querySelector('.jim');
+const michael = document.querySelector('.michael');
+const stanleyAudio = document.querySelector('.stanley-mp4');
+const dwightAudio = document.querySelector('.dwight-mp4');
+const jimAudio = document.querySelector('.jim-mp4');
+const michaelAudio = document.querySelector('.michael-mp4');
 
 
 theme.addEventListener('click', () => {
@@ -12,18 +18,51 @@ theme.addEventListener('click', () => {
 });
 
 alertButton.addEventListener('click', () => {
-  themeSong.pause();
-  themeSong.currentTime = 0;
-  audio.currentTime = 0;
-  audio.play();
+  if (themeSong.currentTime > 0) {
+    themeSong.pause();
+    themeSong.currentTime = 0;
+  } else if (themeSong.currentTime === 0) {
+    themeSong.play();
+  }
 })
 
-themeButton.forEach(button => button.addEventListener('click', () => {
-  audio.pause();
-  audio.currentTime = 0;
-  themeSong.currentTime = 0;
-  themeSong.play();
-}))
+stanley.addEventListener('click', () => {
+  if (stanleyAudio.currentTime > 0) {
+    stanleyAudio.pause();
+    stanleyAudio.currentTime = 0;
+  } else if (stanleyAudio.currentTime === 0) {
+    stanleyAudio.play();
+  }
+})
+
+michael.addEventListener('click', () => {
+  if (michaelAudio.currentTime > 0) {
+    michaelAudio.pause();
+    michaelAudio.currentTime = 0;
+  } else if (michaelAudio.currentTime === 0) {
+      michaelAudio.play();
+    }
+  })
+
+  jim.addEventListener('click', () => {
+    if (jimAudio.currentTime > 0) {
+      jimAudio.pause();
+      jimAudio.currentTime = 0;
+    } else if (jimAudio.currentTime === 0) {
+        jimAudio.play();
+    }
+  })
+
+  dwight.addEventListener('click', () => {
+    if (dwightAudio.currentTime > 0) {
+      dwightAudio.pause();
+      dwightAudio.currentTime = 0;
+    } else if (dwightAudio.currentTime === 0) {
+        dwightAudio.play();
+    }
+  })
+  
+
 
 
 
