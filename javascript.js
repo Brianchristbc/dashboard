031019283afe7947fc3325a8f607bb92c87ba333
Brianchristbc <138142173+Brianchristbc@users.noreply.github.com>
2023-09-30 23:10:22 +0800

    Embed audio easter eggs into webpage

--- javascript.js
+++ javascript.js
@@ -2,17 +2,28 @@ const root = document.documentElement;
 
 const theme = document.querySelector('.set-theme');
 const alertButton = document.querySelector('.alert-button');
+const themeButton = document.querySelectorAll('button');
+const audio = document.querySelector('.alert-sound');
+const themeSong = document.querySelector('.theme-song');
 
 
 theme.addEventListener('click', () => {
     root.classList.toggle('dark');
 });
 
-window.addEventListener('click', () => {
-  const audio = document.querySelector('.alert-sound');
+alertButton.addEventListener('click', () => {
+  themeSong.pause();
+  themeSong.currentTime = 0;
   audio.currentTime = 0;
   audio.play();
 })
+
+themeButton.forEach(button => button.addEventListener('click', () => {
+  audio.pause();
+  audio.currentTime = 0;
+  themeSong.currentTime = 0;
+  themeSong.play();
+}))
 
 
 
